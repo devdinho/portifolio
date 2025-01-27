c656cd956c64bea8be656a7259a49cc83384c4ca
Anderson Freitas <freitas.dev@proton.me>
2025-01-26 22:00:00 -0300

    🔥 Update links
    Update links

--- src/app/environments/environments.ts
+++ src/app/environments/environments.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
-    portifolioApiRoot: 'https://portifolio.freitascodes.me/',
-    apiBibliotecas: 'https://portifolio.freitascodes.me/api/projetos?type=2',
-    apiProjetos: 'https://portifolio.freitascodes.me/api/projetos',
-    apiCertificados: 'https://portifolio.freitascodes.me/api/certificados',
+    portifolioApiRoot: 'https://portifolio.dinho.dev/',
+    apiBibliotecas: 'https://portifolio.dinho.dev/api/projetos?type=2',
+    apiProjetos: 'https://portifolio.dinho.dev/api/projetos',
+    apiCertificados: 'https://portifolio.dinho.dev/api/certificados',
 }
